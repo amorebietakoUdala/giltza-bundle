7d03c344429647a5c98b9793587cff78a085d9b1
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2026-04-22 13:01:47 +0200

    Removed a deprecation on SF 7.4

--- src/DependencyInjection/GiltzaExtension.php
+++ src/DependencyInjection/GiltzaExtension.php
@@ -5,7 +5,7 @@ namespace AMREU\GiltzaBundle\DependencyInjection;
 use AMREU\GiltzaBundle\Controller\GiltzaController;
 use AMREU\GiltzaBundle\Service\GiltzaProvider;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 
